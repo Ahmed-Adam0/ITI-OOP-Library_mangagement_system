5127f1d5b45ac68d632ce30d60fc708202157470
hajar710 <hajarismail2030@gmail.com>
2026-01-20 22:16:30 +0200

    Refactor Book class with encapsulation and new methods
    Refactored the Book class to use private fields and added a constructor for initialization. Introduced public getter methods for ID, Title, Author, and availability status. Implemented a ToString() override for formatted output. Changed namespace to LibraryManagementSystem and set class as internal for better encapsulation.

--- LibraryManagementSystem/Classes/Book.cs
+++ LibraryManagementSystem/Classes/Book.cs
@@ -1,8 +1,40 @@
-using System;
-
-namespace LibraryManagementSystem.Classes;
-
-public class Book
+namespace LibraryManagementSystem
 {
+    internal class Book
+    {
+        private int ID;
+        private string Title;
+        private string Author;
+        private bool IsAvailable;
 
+        // Constructor to initialize a Book object
+        public Book(int id, string title, string author)
+        {
+            ID = id;
+            Title = title;
+            Author = author;
+            IsAvailable = true;
+        }
+        //if you needed  access to private members use this
+        public int GetID()
+        {
+            return ID;
+        }
+        public string GetTitle()
+        {
+            return Title;
+        }
+        public string GetAuthor()
+        {
+            return Author;
+        }
+        public bool CheckAvailability()
+        {
+            return IsAvailable;
+        }
+        public override string ToString()
+        {
+            return $"Book ID: {ID}, Title: {Title}, Author: {Author}, Available: {IsAvailable} \n";
+        }
+    }
 }
