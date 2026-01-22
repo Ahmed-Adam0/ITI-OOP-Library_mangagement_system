742552450069fb83adbc49f1635c3cf886c7d4a1
sahar <sahar@LAPTOP-JR0CTTM9>
2026-01-22 18:45:50 +0200

    Class Member Done

--- LibraryManagementSystem/Classes/Member.cs
+++ LibraryManagementSystem/Classes/Member.cs
@@ -4,4 +4,55 @@ namespace LibraryManagementSystem.Classes;
 
 public class Member
 {
+    private int ID;
+    private string Name;
+    private List<Book> BorrowedBooks = new List<Book>();
+
+    public Member(int ID,string Name)
+    {
+        this.ID = ID;
+        this.Name = Name;
+    }
+    //if you needed  access to private members use this
+
+    public void SetID(int id)
+    {
+            ID = id;
+    }
+    public void SetName(string name)
+    {
+        if (name != null && name.Length > 0)
+            Name = name;
+    }
+  
+
+    public int GetID()
+    {
+        return ID;
+    }
+    public string GetName()
+    {
+        return Name;
+    }
+  
+
+    //if you needed  to add book or remove it from BorrowedBooks list
+    public void AddBook (Book book)
+    {
+        BorrowedBooks.Add(book);
+    }
+    public void RemoveBook(Book book)
+    {
+        BorrowedBooks.Remove(book);
+    }
+    //if you need to see the BorrowedBooks 
+
+    //public string ShowBorrowedBooks()
+    //{
+    //    foreach (Book book in BorrowedBooks) 
+    //    {
+    //       return book.GetTitle();
+    //    }
+    //}
+
 }
